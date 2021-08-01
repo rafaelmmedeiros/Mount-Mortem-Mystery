c5462b752c3ccd443f75b36b2155f98ffefff93f
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-31 22:53:08 -0300

    Preparation

--- Assets/Scripts/Preparation.cs
+++ Assets/Scripts/Preparation.cs
@@ -7,7 +7,7 @@ public class Preparation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Teste
+        // Teste 2
     }
 
     // Update is called once per frame
