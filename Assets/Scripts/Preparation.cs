cde173eef7a71e03e1260df56bb609769ab69fca
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-31 23:51:37 -0300

    SandBox Dev Level

--- Assets/Scripts/Preparation.cs
+++ Assets/Scripts/Preparation.cs
@@ -7,7 +7,7 @@ public class Preparation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Teste 3
+        // Teste 3 
     }
 
     // Update is called once per frame
