05d7181478333bb842f9dd427ad1b02274ab39fa
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-06 00:57:21 -0300

    Cinematic Controller

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        void DisableControl()
+        {
+            print("Disabled-Controller");
+        }
+
+        void EnableControl()
+        {
+            print("Enabled-Controller");
+        }
+
+
+    }
+}
+
