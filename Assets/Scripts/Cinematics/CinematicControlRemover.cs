7dad7d61bc515a08fbd5a27dab7b7882f7ec98cd
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-06 11:57:49 -0300

    Application of Observer Pattern using events

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -1,24 +1,32 @@
+using RPG.Control;
+using RPG.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace RPG.Cinematics
 {
     public class CinematicControlRemover : MonoBehaviour
     {
+        GameObject player;
+
         private void Start()
         {
-            
+            player = GameObject.FindWithTag("Player");
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
         }
 
-        void DisableControl()
+        void DisableControl(PlayableDirector playableDirector)
         {
-            print("Disabled-Controller");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
-        void EnableControl()
+        void EnableControl(PlayableDirector playableDirector)
         {
-            print("Enabled-Controller");
+            player.GetComponent<PlayerController>().enabled = true;
         }
 
 
