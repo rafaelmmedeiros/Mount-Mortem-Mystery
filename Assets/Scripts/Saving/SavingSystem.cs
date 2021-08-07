8aa7fa7fe0293304ee502d06135fc3a6b2498eda
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-07 00:51:12 -0300

    Save System

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -8,12 +10,22 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to file: " + saveFile);
+            string path = GetPathfromSaveFile(saveFile);
+            print("Saving to: " + path);
+            FileStream fileStream = File.Open(path, FileMode.Create);
+            byte[] bytes = Encoding.UTF8.GetBytes("We are doomed");
+            fileStream.Write(bytes, 0, bytes.Length);
+            fileStream.Close();
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from file: " + saveFile);
+            print("Loading from: " + GetPathfromSaveFile(saveFile));
+        }
+
+        private string GetPathfromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
 }
