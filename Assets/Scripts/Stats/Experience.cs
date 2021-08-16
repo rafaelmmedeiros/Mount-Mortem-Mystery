2e3bdef5120fb1f6e8ddb2d1e249c9b613497ebc
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-16 16:31:01 -0300

    UI / Stats

--- Assets/Scripts/Stats/Experience.cs
+++ Assets/Scripts/Stats/Experience.cs
@@ -1,14 +1,32 @@
+using RPG.Saving.Interfaces;
 using UnityEngine;
 
 namespace RPG.Stats
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public float GetExperiencePoints()
+        {
+            return experiencePoints;
+        }
+
+
+        //  INTERFACES
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
---
+++ Assets/Scripts/UI/ExperienceDisplay.cs
@@ -0,0 +1,23 @@
+using RPG.Attributes;
+using RPG.Stats;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.UI
+{
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        Experience experience;
+
+        private void Awake()
+        {
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+        }
+
+        private void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", experience.GetExperiencePoints());
+        }
+    }
+}
