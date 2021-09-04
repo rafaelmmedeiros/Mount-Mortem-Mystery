91f95892498962b3570a7d91598ae5870b400464
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-09-04 16:31:33 -0300

    UI

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        private void Start()
+        {
+            Spawn(11);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+    }
+}
